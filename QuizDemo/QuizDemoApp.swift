d1ad3e7110f88368b9f0419cac1805c788ac0971
fatihahmet06 <fatihahmet06@hotmail.com>
2022-06-10 18:08:35 +0300

    Initial Commit

---
+++ QuizDemo/QuizDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizDemoApp.swift
+//  QuizDemo
+//
+//  Created by Fatih Ahmet Gürbüz on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct QuizDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
